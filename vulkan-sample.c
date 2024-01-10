ebf8be975729adf962d42feda873849fdf472d52
Moundier <garamoundier.developer@protonmail.com>
2024-01-10 01:13:35 -0300

    format: ident tab!

--- vulkan-sample.c
+++ vulkan-sample.c
@@ -5,27 +5,27 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define new(value) (value*) malloc(sizeof(value)) // Implicit casting
+#define new(value) (value *)malloc(sizeof(value)) // Implicit casting
 
-const char* WIN_TITLE = "Sample Vulkan";
+const char *WIN_TITLE = "Sample Vulkan";
 const uint32_t WIN_WIDTH = 800;
 const uint32_t WIN_HEIGHT = 600;
 
 typedef struct App {
-  GLFWwindow* window;
+  GLFWwindow *window;
 } App;
 
-void initWindow(App* app);
+void initWindow(App *app);
 void initVulkan(void);
-void eventsLoop(App* app);
-void cleanerOff(App* app);
+void eventsLoop(App *app);
+void cleanerOff(App *app);
 
 int main() {
 
-  App* app = new(App);
+  App *app = new (App);
 
-	// Console debug
-	printf("[INFO]: main funcion startup...\n");
+  // Console debug
+  printf("[INFO]: main funcion startup...\n");
 
   initWindow(app);
   initVulkan();
@@ -34,33 +34,33 @@ int main() {
   return 0;
 }
 
-void initWindow(App* app) {
+void initWindow(App *app) {
   glfwInit();
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
   app->window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL);
-	
-	// Console debug
-	printf("[INFO]: init glfw window...\n");
+
+  // Console debug
+  printf("[INFO]: init glfw window...\n");
 }
 
 void initVulkan(void) { return; }
 
-void eventsLoop(App* app) {
+void eventsLoop(App *app) {
 
-	// Console debug
-	printf("[INFO]: poll events running...\n");
+  // Console debug
+  printf("[INFO]: poll events running...\n");
 
   while (!glfwWindowShouldClose(app->window)) {
     glfwPollEvents(); // Event listener
   }
 }
 
-void cleanerOff(App* app) {
+void cleanerOff(App *app) {
   glfwDestroyWindow(app->window);
   glfwTerminate();
-	
-	// Console debug
-	printf("[INFO]: application destroyed...\n");
+
+  // Console debug
+  printf("[INFO]: application destroyed...\n");
 }
